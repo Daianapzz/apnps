15d52ac3490027cedf394e57d5e89fa70e58de16
daiana <daianapazs105@gmail.com>
2020-11-05 21:06:09 -0200

    criação da rota layout

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/mensagem',function(rec,res){
     res.send("Essa mensagem é automática")
 })
 
+app.get('/layout',function(rec,res){
+    res.send("Esse é o novo layout")
+})
+
 app.listen(3000,function () {
     console.log("conexão inicializada")
 })
