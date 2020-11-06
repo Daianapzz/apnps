a9a584723eb635fe9198f1d243541946949c35c4
daiana <daianapazs105@gmail.com>
2020-11-05 22:57:42 -0200

    configurações iniciais do projeto

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const express = require('express')
 const app = express()
+var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+var path = require('path')
+
+app.use(cookieParser())
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.set("view engine", "ejs")
+
+app.use(express.static(path.join(__dirname, "public")))
 
 app.get('/',function(rec,res){
     res.send("Olá Daiana")
-})
-
-app.get('/mensagem',function(rec,res){
-    res.send("Essa mensagem é automática")
-})
-
-app.get('/sobre',function(rec,res){
-    res.send("Essa página está sendo desenvolvida por Daiana")
-})
-
-app.get('/layout',function(rec,res){
-    res.send("Esse é o novo layout")
 })
 
 app.listen(3000,function () {
